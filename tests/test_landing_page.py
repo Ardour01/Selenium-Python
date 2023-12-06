0ce0e719700c5b783625bbca0ab07f505e4a31c8
ardour.01 <ardour.01@gmail.com>
2023-12-07 01:58:26 +0530

    Fixed landing page test cases and added one new

--- tests/test_landing_page.py
+++ tests/test_landing_page.py
@@ -1,13 +1,14 @@
-import pytest
-import time
 from tests.test_base import TestBase
 import utilities.custom_logger as cl
 
 
 class TestLandingPage(TestBase):
 
+    def test_page_title(self):
+        assert self.landing_obj.page_title() == "Selenium Easy - Best Demo website to practice Selenium Webdriver Online"
+
     def test_landing_page(self):
         assert self.landing_obj.is_displayed_top_navigation_bar() == True
 
     def test_start_practising(self):
-        self.landing_obj.click_start_practising_btn()
+        assert "down" in self.landing_obj.click_start_practising_btn()
